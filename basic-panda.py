7f0d7d5140b3dcdb1d0c05269ab10eb6d6e94ddb
Sirisha Velampalli <sirisha.velampalli@gmail.com>
2017-09-13 04:39:25 +0530

    Creating dataframe
    Creating dataframe using panda

---
+++ basic-panda.py
@@ -0,0 +1,7 @@
+import pandas as py
+
+Student_data={'student1':['java', 'c', 'c++', 'linux'], 'student2': ['python', 'html','javascript','linux'],
+              'student3':['neo4j','perl', 'github', 'panda']}
+
+data1 =py.DataFrame(Student_data)
+print data1
